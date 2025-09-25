3b02f68cc247a8aba069bd80ccbb8e1eea6a0fd5
tou - ThatOneUnoriginal <70731807+ThatOneUnoriginal@users.noreply.github.com>
2025-09-24 22:38:35 -0400

    Add project files.

---
+++ AssignmentOneApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AssignmentOneApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AssignmentOneApplication/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentOneApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
